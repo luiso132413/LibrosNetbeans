72f01049f7e3f14074ecfbd2f67d84a1d218e4de
luiso132413 <loviedog@miumg.edu.gt>
2024-10-02 19:31:40 -0600

    lastcommit

--- app/models/book.model.js
+++ app/models/book.model.js
@@ -1,28 +1,36 @@
-
 module.exports = (sequelize, Sequelize) => {
-	const Book = sequelize.define('book', {	
+	const Book = sequelize.define('book', {
 	  id_book: {
-			type: Sequelize.INTEGER,
-			autoIncrement: true,
-			primaryKey: true
-    },
-	  bookname: {
-			type: Sequelize.STRING
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
 	  },
-	  description: {
-			type: Sequelize.STRING
-  	},
-	  pages: {
-			type: Sequelize.STRING
+	  titulo: {
+		type: Sequelize.STRING
 	  },
-	  author: {
-			type: Sequelize.INTEGER
-    },
-    copyrightby: {
-      type: Sequelize.STRING,
-      defaultValue: 'UMG Antigua'
-    }
+	  autor: {
+		type: Sequelize.STRING
+	  },
+	  isbn: {
+		type: Sequelize.STRING
+	  },
+	  editorial: {
+		type: Sequelize.STRING
+	  },
+	  anio_publicacion: {
+		type: Sequelize.INTEGER
+	  },
+	  categoria: {
+		type: Sequelize.STRING
+	  },
+	  cantidad_disponible: {
+		type: Sequelize.INTEGER
+	  },
+	  ubicacion: {
+		type: Sequelize.STRING
+	  }
 	});
-	
+  
 	return Book;
-}
+  };
+  
